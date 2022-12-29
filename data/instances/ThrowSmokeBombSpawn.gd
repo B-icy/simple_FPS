7be0cd6ee37cde23116e49a5aad14db26184d724
B-icy <beshir.aissi@gmail.com>
2022-12-28 16:09:11 -0800

    a few tweeks!
    glgl

--- data/instances/ThrowSmokeBombSpawn.gd
+++ data/instances/ThrowSmokeBombSpawn.gd
@@ -1,35 +1,22 @@
 extends Spatial
 
+################################################
 
 onready var main = get_tree().current_scene
 #onready var spawnpoint = get_node("../SmokeBomb")
-var smokescreen = load("res://SmokeBomb.tscn")
+var smokescreen = preload("res://SmokeBomb.tscn")
 export var numberofsmokes = 5.0;
 
+################################################
 
-# Called when the node enters the scene tree for the first time.
-func user_input():
-	
-	if numberofsmokes > 0:
-		if Input.is_action_just_pressed("smokebomb"):
-			_loadsmokebomb()
-			numberofsmokes = numberofsmokes - 1;
-	
-	
-	pass # Replace with function body.
+func _physics_process(delta):
+	if Input.is_action_just_pressed("smokebomb") and numberofsmokes > 0:
+		_loadsmokebomb()
+		numberofsmokes = numberofsmokes - 1;
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-	
-	
-	pass
 
 func _loadsmokebomb():
 	var smokescreencopy = smokescreen.instance()
-	smokescreencopy.global_transform.origin = global_transform.origin
-	main.add_child(smokescreencopy)
+	smokescreencopy.global_transform.origin = get_parent().global_transform.origin
+	get_tree().current_scene.add_child(smokescreencopy)
 	yield(get_tree().create_timer(2.0), "timeout")
-	
-	
